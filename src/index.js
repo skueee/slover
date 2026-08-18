d79af87eda9e199ad9e7541af24c4d4f4fda9139
skuee <skueee@outlook.com>
2026-08-18 08:32:13 +0000

    Added video support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { c2paCheck } from "./checkers/c2pa.js"
 
+// Image analysis
 export class ImageAnalysisResult {
   constructor(c2pa) {
     this.c2pa = c2pa
@@ -37,4 +38,45 @@ export class ImageAnalysisResult {
 export async function imageAnalysis(path) {
   const c2pa = await c2paCheck(path)
   return new ImageAnalysisResult(c2pa)
+}
+
+
+// Video analysis
+export class VideoAnalysisResult {
+  constructor(c2pa) {
+    this.c2pa = c2pa
+  }
+
+  get getFlaggedList() {
+    const result = []
+    if (this.c2pa.flagged == true) { result.push("c2pa") }
+
+    return result
+  }
+
+  get getFlaggedCount() {
+    const result = 0
+    if (this.c2pa.flagged == true) { result++ }
+  }
+
+  get isFlagged() {
+    if (this.c2pa.flagged == true) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  get authorsList() {
+    const result = []
+    if (this.c2pa.flagged == true) {
+      result.push(this.c2pa.manifest.signature_info.issuer)
+    }
+    return result
+  }
+}
+
+export async function videoAnalysis(path) {
+  const c2pa = await c2paCheck(path)
+  return new VideoAnalysisResult(c2pa)
 }
